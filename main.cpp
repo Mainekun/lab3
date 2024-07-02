55cb403859c42fcd96bcabf311d769cdbe6d9aad
Mainekun <130379136+Mainekun@users.noreply.github.com>
2024-07-02 19:51:44 +0300

    code: гистограмма строится на основе кол-ва корзин alignment: теперь гистограмма имеет фиксированный отступ

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <map>
+#include <vector>
 #include <cmath>
 
 using namespace std;
 
-const size_t WIDTH = 80;    
+const size_t WIDTH = 80;
 const size_t MAX_ASTERICS = WIDTH - 4;
 
 int main() {
@@ -15,6 +16,10 @@ int main() {
     cerr << "Enter number count: ";
     cin >> num_count;
 
+    size_t bin_count;
+    cerr << "Enter bin count: ";
+    cin >> bin_count;
+
     cerr << "Enter step from 4 to 9: ";
     cin >> step;
     if (step < 4 || step > 9) {
@@ -22,66 +27,62 @@ int main() {
         return 1;
     }
 
-    map<size_t,size_t> bin_values;
-    for (size_t i = 0; i < num_count; i++) {
-        int temp; 
-        cin >> temp;
-        if (bin_values.find(temp) == bin_values.end())
-            bin_values[temp] = 1;
-        else
-            bin_values[temp]++;
-    }
+    vector<size_t> nums(num_count);
+    for (size_t i = 0; i < num_count; i++)
+        cin >> nums[i];
 
-    
 
     //------------------
     //Обработка значений
     //------------------
-    size_t max_count = (bin_values.begin())->second;
-    size_t max_num = (bin_values.begin())->first;
-    for (auto i = ++bin_values.begin(); i != bin_values.end(); i++) {
-        max_count = i->second > max_count ? i->second : max_count;
-        max_num = i->first > max_num ? i->first : max_num;
+    size_t max_n = nums[0];
+    size_t min_n = nums[0];
+    for (auto i = ++nums.begin(); i != nums.end(); i++) {
+        max_n = *i > max_n ? *i : max_n;
+        min_n = *i < min_n ? *i : min_n;
+    }
+
+    double step_bin = static_cast<double>(max_n - min_n) / bin_count;
+    vector<size_t> bins(bin_count, 0);
+    for (auto i : nums) {
+        for (size_t j = 0; j < bin_count; j++) {
+            if ((i >= min_n + step_bin * j && i < min_n + step_bin * (j + 1)) || j == bin_count - 1) {
+                bins[j]++;
+                break;
+            }
+        }
     }
 
 
-    
+
     //--------------
     //Вывод значений
     //--------------
-    #define bin_num bin.second
-    for (auto bin : bin_values) {
-        if (max_count >= 10 && max_count < 100) {
-            if (bin_num < 10) cout << " ";
-        } 
-        else if (max_count >= 100) {
-            if (bin_num < 10) cout << "  ";
-            else if (bin_num < 100) cout << " ";
-        }
-        cout << bin_num; 
+    for (auto bin_val : bins) {
+        if (bin_val < 100) cout << " ";
+        if (bin_val < 10) cout << " ";
+        cout << bin_val;
         cout << "|";
 
         int astericses;
-        if (max_count > MAX_ASTERICS) 
-            astericses = MAX_ASTERICS * (static_cast<double>(bin.second) / max_count);
-        else astericses = bin.second; 
+        if (max_n > MAX_ASTERICS)
+            astericses = MAX_ASTERICS * (static_cast<double>(bin_val) / max_n);
+        else astericses = bin_val;
         for (size_t i = 0; i < astericses || i == 0; i++) {
             cout << '*';
         }
         cout << '\n';
     }
 
-      
+
     //  +-------+  //
     //  |Подписи|  //
     //  +-------+  //
     //шкала
-    if (max_count > 100) cout << "   |";
-    else if (max_count > 10) cout << "  |";
-    else cout << " |";
+    cout << "   |";
 
     for (size_t i = 0; i < MAX_ASTERICS; i += step) {
-        for (size_t j = 1; (j < step) ; j++) {
+        for (size_t j = 1; (j < step); j++) {
             if (i + j >= MAX_ASTERICS) break;
             cout << "-";
         }
@@ -89,15 +90,13 @@ int main() {
     }
     cout << "\n";
 
-    if (max_count > 100) cout << "   0";
-    else if (max_count > 10) cout << "  0";
-    else cout << " 0";
+    cout << "   0";
 
     //подписи
-    double point = max_count > MAX_ASTERICS
-                   ? static_cast<double>(max_count) 
-                   / MAX_ASTERICS : 1;
-    for (size_t i = 0; i < MAX_ASTERICS-1; i++) {
+    double point = max_n > MAX_ASTERICS
+        ? static_cast<double>(max_n)
+        / MAX_ASTERICS : 1;
+    for (size_t i = 0; i < MAX_ASTERICS - 1; i++) {
         if (i == step - 1) {
             size_t second_num = round(point * step);
             cout << second_num;
