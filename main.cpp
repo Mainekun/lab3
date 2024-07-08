31fa4dbf3c27788a4e091767cb924df2415603d9
Mainekun <130379136+Mainekun@users.noreply.github.com>
2024-07-08 23:14:46 +0300

    code: decomposed

--- main.cpp
+++ main.cpp
@@ -8,98 +8,66 @@ using namespace std;
 const size_t WIDTH = 80;
 const size_t MAX_ASTERICS = WIDTH - 4;
 
-int main() {
-    //-------------
-    //Ввод значений
-    //-------------
-    size_t num_count, step;
-    cerr << "Enter number count: ";
-    cin >> num_count;
-
-    size_t bin_count;
-    cerr << "Enter bin count: ";
-    cin >> bin_count;
-
-    cerr << "Enter step from 4 to 9: ";
-    cin >> step;
-    if (step < 4 || step > 9) {
-        cout << "ERROR";
-        return 1;
-    }
-
-    vector<size_t> nums(num_count);
-    for (size_t i = 0; i < num_count; i++)
+vector<size_t> input_numbers(size_t count) {
+    vector<size_t> nums(count);
+    for (size_t i = 0; i < count; i++) 
         cin >> nums[i];
+    return nums; 
+}
 
+void find_min_max(const vector<size_t> nums, size_t& max_num, size_t& min_num) {
+    max_num = *max_element(nums.begin(), nums.end());
+    min_num = *min_element(nums.begin(), nums.end());
+}
 
-    //------------------
-    //Обработка значений
-    //------------------
-    size_t max_n = nums[0];
-    size_t min_n = nums[0];
-    for (auto i = ++nums.begin(); i != nums.end(); i++) {
-        max_n = *i > max_n ? *i : max_n;
-        min_n = *i < min_n ? *i : min_n;
-    }
+vector<size_t> make_histogram
+(const vector<size_t> nums, const size_t min_num, const size_t max_num, const size_t bin_count) {
+    double step = static_cast<double>(max_num - min_num) / bin_count;
+    vector<size_t> bins(bin_count);
+    for (auto num : nums) 
+        for (size_t i = 0; i < bin_count; i++)
+            if ((num >= min_num + step * i && num < min_num + step * (i + 1)) ||
+                i == bin_count) {
+                    bins[i]++;
+                    break;
+                }
 
-    double step_bin = static_cast<double>(max_n - min_n) / bin_count;
-    vector<size_t> bins(bin_count, 0);
-    for (auto i : nums) {
-        for (size_t j = 0; j < bin_count; j++) {
-            if ((i >= min_n + step_bin * j && i < min_n + step_bin * (j + 1)) || j == bin_count - 1) {
-                bins[j]++;
-                break;
-            }
-        }
-    }
+    return bins;
+}
 
-
-    //--------------
-    //Вывод значений
-    //--------------
+void print_histogram(vector<size_t> bins, size_t step, size_t max_num) {
     size_t max_count = *max_element(bins.begin(), bins.end());
-    for (auto bin_val : bins) {
-        if (bin_val < 100) cout << " ";
-        if (bin_val < 10) cout << " ";
-        cout << bin_val;
-        cout << "|";
+    for (auto bin : bins) {
+        if (bin < 100) cout << ' ';
+        if (bin < 10) cout << ' ';
+        cout << bin << '|';
 
-        int astericses;
+        size_t asterics;
         if (max_count > MAX_ASTERICS)
-            astericses = MAX_ASTERICS * (static_cast<double>(bin_val) / max_count);
-        else astericses = bin_val;
-        for (size_t i = 0; i < astericses || i == 0; i++) {
+            asterics = MAX_ASTERICS * (static_cast<double>(bin) / max_count);
+        else asterics = bin;
+        for (size_t i = 0 ; i < asterics; i++) 
             cout << '*';
-        }
         cout << '\n';
     }
 
-
-    //  +-------+  //
-    //  |Подписи|  //
-    //  +-------+  //
-    //шкала
     cout << "   |";
-    size_t floor = max_count > MAX_ASTERICS ? MAX_ASTERICS : max_count;
+    size_t floor = max(max_count, MAX_ASTERICS);
     size_t steps_count = ceil(static_cast<double>(floor) / step);
     if (steps_count == 1) steps_count++;
 
-    for (size_t i = 0; i < steps_count && (i * step) < floor; i++) {
-        for (size_t j = 1; j < step; j++) {
-            cout << "-";
-        }
-        cout << "|";
-    }
-    cout << "\n";
-
+    for (size_t i = 0; (i < steps_count) && (i * step < floor); i++)
+        for (size_t j = 1; j < step; j++) 
+            cout << '-';
+        cout << '|';
+    cout << '\n';
     cout << "   0";
 
-    //подписи
-    double point = max_n > MAX_ASTERICS
-        ? static_cast<double>(max_n) / MAX_ASTERICS
+    double point = max_num > MAX_ASTERICS
+        ? static_cast<double>(max_num) / MAX_ASTERICS
         : 1;
     for (size_t i = 0; i < steps_count * step - 1 || i < 2 * step - 1; i++) {
-        if (i == step - 1) {
+        if (i == step - 1) { 
             size_t second_num = round(point * step);
             cout << second_num;
             if (second_num > 10) i++;
@@ -107,7 +75,26 @@ int main() {
             continue;
         }
 
-        cout << " ";
+        cout << ' ';
     }
     cout << round(point * max_count);
+}
+
+int main() 
+{
+    size_t num_count, bin_count, step, min_num, max_num;
+    cerr << "Enter number count: ";
+    cin >> num_count; 
+    cerr << "Enter bin count: ";
+    cin >> bin_count; 
+    cerr << "Enter step (4 <= x <= 9): ";
+    cin >> step;
+    if (step < 4 || step > 9) {
+        cout << "ERROR";
+        return 1;
+    }
+    const auto nums = input_numbers(num_count);
+    find_min_max(nums, max_num, min_num);
+    const auto bins = make_histogram(nums, min_num, max_num, bin_count);
+    print_histogram(bins, step, max_num);
 }
